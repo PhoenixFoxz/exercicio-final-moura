63144a915fd01b10abfa842b85ca985fb9e9c8b4
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-02-01 11:46:46 -0300

    Criação dos posts

--- src/pages/company.jsx
+++ src/pages/company.jsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Link from "next/link";
+import styled from "styled-components";
 
 export default function Company() {
   return (
@@ -10,6 +12,69 @@ export default function Company() {
       <section>
         <h2>Conheça as Free Company do Final Fantasy XIV!</h2>
       </section>
+      <StyledListaPerso>
+        <article>
+          <Link href="/">
+            <h3>Título do post...</h3>
+            <p>Subtítulo do post...</p>
+          </Link>
+        </article>
+        <article>
+          <Link href="/">
+            <h3>Título do post...</h3>
+            <p>Subtítulo do post...</p>
+          </Link>
+        </article>
+        <article>
+          <Link href="/">
+            <h3>Título do post...</h3>
+            <p>Subtítulo do post...</p>
+          </Link>
+        </article>
+        <article>
+          <Link href="/">
+            <h3>Título do post...</h3>
+            <p>Subtítulo do post...</p>
+          </Link>
+        </article>
+      </StyledListaPerso>
     </>
   );
 }
+
+const StyledListaPerso = styled.div`
+  article {
+    background-color: #f7f7f7;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    box-shadow: var(--sombra-box);
+    border-radius: var(--borda-arredondada);
+    transition: transform 200ms;
+  }
+
+  article:hover {
+    cursor: pointer;
+    transform: scale(1.05);
+  }
+
+  @media screen and (min-width: 500px) {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    article {
+      width: 49%;
+    }
+  }
+
+  & a {
+    text-decoration: none;
+    color: black;
+
+    &:hover,
+    &:focus {
+      color: #1a18a7;
+      transition: 500ms;
+    }
+  }
+`;
