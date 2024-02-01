bc8071f2c101efe660408bd668258888f9ed8eb4
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-02-01 08:17:39 -0300

    Resetando o projeto

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="/images/favicon.png"
+          type="image/png"
+          sizes="256x256"
+        />
+        <meta name="author" content="Felipe Moura Martins"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
