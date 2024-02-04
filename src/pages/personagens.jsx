0e4e82bd7444b739f77587e9feb25e6d9c5b1589
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-02-04 18:38:23 -0300

    Em construção

--- src/pages/personagens.jsx
+++ src/pages/personagens.jsx
@@ -15,7 +15,11 @@ export default function Personagens() {
       <section>
         <h2>Personagens do Final Fantasy XIV!</h2>
       </section>
-      <StyledListaPerso>
+      <h3 style={{textAlign: "center"}}>EM CONSTRUÇÃO!</h3>
+      <StyledImg>
+        <img src="/images/construcao.webp" alt="Ilustração de um Moogle" />
+      </StyledImg>
+      {/* <StyledListaPerso>
         <article>
           <Link href="/">
             <h3>Título do post...</h3>
@@ -40,44 +44,64 @@ export default function Personagens() {
             <p>Subtítulo do post...</p>
           </Link>
         </article>
-      </StyledListaPerso>
+      </StyledListaPerso> */}
     </>
   );
 }
 
-const StyledListaPerso = styled.div`
-  article {
-    background-color: #f7f7f7;
-    padding: 1rem;
-    margin-bottom: 1rem;
-    box-shadow: var(--sombra-box);
-    border-radius: var(--borda-arredondada);
-    transition: transform 200ms;
+
+
+// const StyledListaPerso = styled.div`
+//   article {
+//     background-color: #f7f7f7;
+//     padding: 1rem;
+//     margin-bottom: 1rem;
+//     box-shadow: var(--sombra-box);
+//     border-radius: var(--borda-arredondada);
+//     transition: transform 200ms;
+//   }
+
+//   & div {
+//     text-align: center;
+//   }
+
+//   h3 {
+//     text-align: center;
+//   }
+
+//   article:hover {
+//     cursor: pointer;
+//     transform: scale(1.05);
+//   }
+
+//   @media screen and (min-width: 500px) {
+//     display: flex;
+//     justify-content: space-between;
+//     flex-wrap: wrap;
+
+//     article {
+//       width: 49%;
+//     }
+//   }
+
+//   & a {
+//     text-decoration: none;
+//     color: black;
+
+//     &:hover,
+//     &:focus {
+//       color: #1a18a7;
+//       transition: 500ms;
+//     }
+//   }
+// `;
+
+const StyledImg = styled.div`
+  text-align: center;
+  img {
+    width: 50%;
   }
-
-  article:hover {
-    cursor: pointer;
-    transform: scale(1.05);
+  @media screen and (max-width: 500px) {
+    img {display: none;}
   }
-
-  @media screen and (min-width: 500px) {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-
-    article {
-      width: 49%;
-    }
-  }
-
-  & a {
-    text-decoration: none;
-    color: black;
-
-    &:hover,
-    &:focus {
-      color: #1a18a7;
-      transition: 500ms;
-    }
-  }
-`;
+`
